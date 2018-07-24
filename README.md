bac4a38a80c1e176c1924e4ae08459c8dc673b68
Pho Huynh <shortgiraffe4@gmail.com>
2018-07-25 00:21:19 +0700

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+Kết quả sau khi set up từ bài viết 
+<https://blog.phohuynh.com/2018/07/25/webpack-4-tu-co-ban-cho-den-xay-dung-project-react-es-6-build-production.html>
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>React Component</div>
+    );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
